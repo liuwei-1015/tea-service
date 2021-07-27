efa65feaa904bcaa4a154e69ae98dc0b6434f7bc
刘伟 <liuwei1306341015@163.com>
2021-07-27 10:32:48 +0000

    update app/service/user.js.

--- app/service/user.js
+++ app/service/user.js
@@ -4,14 +4,29 @@ class UserService extends Service {
   async add () {
     const { ctx } = this
     const { userName, nickName, passWord } = ctx.request.body
-    const result = await ctx.model.User.create({
-      user_name: userName,
-      nick_name: nickName,
-      pass_word: passWord
-    }, (err) => {
-      this.logger.warn(err)
-    })
-    return result
+    const result = await ctx.model.User.find({ 'user_name': userName })
+    if (result.length) {
+      return {
+        code: -1,
+        message: '该用户名已经注册！'
+      }
+    } else {
+      const info = await ctx.model.User.create({
+        user_name: userName,
+        nick_name: nickName,
+        pass_word: passWord
+      })
+      if (info) {
+        return {
+          code: 0,
+          message: '注册成功！'
+        }
+      }
+      return {
+        code: -1,
+        message: '注册失败！'
+      }
+    }
   }
 }
 
